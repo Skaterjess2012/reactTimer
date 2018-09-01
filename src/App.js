51834e4f5bd2e387bef314a9f99b7c59d3ff6c0c
Jesse Garcia <jeg6187@gmail.com>
2018-09-01 12:25:02 -0600

    adding a few things that i did last nighyt

--- src/App.js
+++ src/App.js
@@ -4,46 +4,92 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 
 class Timer extends Component {
-  timerState = false;
   constructor(props) {
     super(props);
     this.state = {
       title: 'title',
       subTitle: 'sub-title',
-      counter: 0,
-      timer: null
+      secondCount: 60,
+      minuteCount: 60,
+      hourCount: 0
     };
+  }
+  componentWillUnmount () {
+    clearInterval(this.timer);
+  }
+  tick () {
+    if (this.state.secondCount === 1) {
+      this.setState({
+        secondCount: 60,
+        minuteCount: this.state.minuteCount - 1
+      });
+    } else {
+      this.setState({secondCount: (this.state.secondCount - 1)});
+    }
+    if (this.state.minuteCount === 0) {
+      this.setState({
+        minuteCount: 60,
+        hourCount: this.state.hourCount + 1
+      });
+    }
+  }
+  startTimer () {
+    clearInterval(this.timer)
+    this.timer = setInterval(this.tick.bind(this), 1000);
+  }
+  stopTimer () {
+    clearInterval(this.timer);
+  }
+  displayNum(timerCount, type) {
+    let tempNum;
+    if (type === 'second') {
+      tempNum = 60 - timerCount;
+      if (tempNum === 0) {
+        return '00';
+      }
+    }
+    if (type === 'minute') {
+      tempNum = 60 - timerCount;
+    }
+    if (type === 'hour') {
+      tempNum = timerCount;
+    }
+    if (tempNum < 10) {
+      return '0' + tempNum;
+    }
+    return tempNum;
+  }
+  updateTitleInput(evt) {
+    this.setState({
+      title: evt.target.value
+    })
+  }
+  updateSubTitleInput(evt) {
+    this.setState({
+      subTitle: evt.target.value
+    })
+  }
+  timerEdit() {
   }
   render() {
     return(
     <Card className="timerCard">
       <div className="timerTitle">
-        <h2 className="Title">{this.state.title}</h2>
-        <h4 className="SubTitle">{this.state.subTitle}</h4>
+        <h2 className="title">{this.state.title}</h2>
+        <input className="input inputTitle" type="text" placeholder="Title" value={this.state.inputValue} onChange={evt => this.updateTitleInput(evt)} />
+        <h4 className="subTitle">{this.state.subTitle}</h4>
+        <input className="input inputSubTitle" type="text" placeholder="Sub Title" value={this.state.inputValue} onChange={evt => this.updateSubTitleInput(evt)} />
       </div>
-      <h1>{this.state.timer}</h1>
+      <h1>{`${this.displayNum(this.state.hourCount, 'hour')}:${this.displayNum(this.state.minuteCount, 'minute')}:${this.displayNum(this.state.secondCount, 'second')}`}</h1>
       <div className="timerButtons">
         <Button color="primary" className="button timerDelete">Delete</Button>
-        <Button color="primary" className="button timerEdit">Edit</Button>
       </div>
-      <Button onClick={this.startStopTimer}>Start</Button>
+      <div className="timerStartStop">
+        <Button onClick={this.startTimer.bind(this)}>Start</Button>
+        <Button onClick={this.stopTimer.bind(this)}>Stop</Button>
+      </div>
     </Card>
     )
-  }
-  tick() {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  }
-  startStopTimer() {
-    if (this.timerState) {
-      this.clearInterval(this.state.timer);
-      this.timerState = false;
-    } else {
-      let timer = setInterval(this.tick(), 1000);
-      this.setState({timer});
-      this.timerState = true;
-    }
   }
 }
 
